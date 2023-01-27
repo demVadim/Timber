3ce7176478ee7bd752b1f57fd4583580f2cdecf1
demVadim <demmed85@mail.ru>
2023-01-27 15:43:15 +0800

    add

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     sf::VideoMode vm(1920, 1080);
     sf::RenderWindow window(vm, "Timber!!!", sf::Style::Fullscreen);
 
-
+    //make Fon
     sf::Texture textureBackground;
     textureBackground.loadFromFile("graphics/background.png");
 
@@ -13,7 +13,47 @@ int main()
     spriteBackground.setTexture(textureBackground);
     spriteBackground.setPosition(0,0);
 
+    //make Tree sprite
+    sf::Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
 
+    sf::Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810,0);
+
+    //make Bee
+    sf::Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+
+    sf::Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0,800);
+
+    bool beeActive = false;
+    float beeSpeed = 0.0f;
+
+    //make 3 Cloud
+    sf::Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+
+    sf::Sprite spriteCloud1;
+    sf::Sprite spriteCloud2;
+    sf::Sprite spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+
+    spriteCloud1.setPosition(0,0);
+    spriteCloud2.setPosition(0,250);
+    spriteCloud3.setPosition(0,500);
+
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
 
 
     while (window.isOpen())
@@ -26,7 +66,11 @@ int main()
             window.clear();
 
             window.draw(spriteBackground);
-
+            window.draw(spriteCloud1);
+            window.draw(spriteCloud2);
+            window.draw(spriteCloud3);
+            window.draw(spriteTree);
+            window.draw(spriteBee);
             window.display();
     }
 
