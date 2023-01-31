2de294f714ebef02d3cca56bd4de049ad049361c
demVadim <demmed85@mail.ru>
2023-01-31 16:53:36 +0800

    cloud completed and add pause value

--- main.cpp
+++ main.cpp
@@ -57,8 +57,14 @@ int main()
 
     sf::Clock clock;
 
+    // Track whether the game is running
+    bool paused = true;
+
     while (window.isOpen())
     {
+
+        //Handle the players input
+
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
         {
             window.close();
@@ -88,16 +94,16 @@ int main()
                 spriteBee.setPosition(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()), spriteBee.getPosition().y);
 
                 // Has the bee reached the left-hand edge of the screen?
-                if (spriteBee.getPosition().x < -100)
-                {
+            if (spriteBee.getPosition().x < -100)
+            {
                     // Set it up ready to be a whole new bee next frame
-                    beeActive = false;
-                }
+                beeActive = false;
+            }
             }
 
             // Manage clouds
             // Cloud 1
-            if (!cloud1Speed)
+            if (!cloud1Active)
             {
                 //How fast is the cloud
                 srand((int)time(0) * 10);
@@ -111,15 +117,62 @@ int main()
             }
             else
             {
-            spriteCloud1.setPosition(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()),spriteCloud1.getPosition().y);
-            // Has the cloud reached the right hand edge of the screen?
-            if (spriteCloud1.getPosition().x > 1920)
-            {
-            // Set it up ready to be a whole new cloud next frame
-            cloud1Active = false;
-            }
+                spriteCloud1.setPosition(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()),spriteCloud1.getPosition().y);
+                // Has the cloud reached the right hand edge of the screen?
+                if (spriteCloud1.getPosition().x > 1920)
+                {
+                // Set it up ready to be a whole new cloud next frame
+                cloud1Active = false;
+                }
             }
 
+            // Cloud 2
+            if (!cloud2Active)
+            {
+                //How fast is the cloud
+                srand((int)time(0) * 20);
+                cloud2Speed = (rand() % 200);
+
+                // How high is the cloud
+                srand((int)time(0) * 20);
+                float height = (rand() % -300) - 150;
+                spriteCloud2.setPosition(-200, height);
+                cloud2Active = true;
+            }
+            else
+            {
+                spriteCloud2.setPosition(spriteCloud2.getPosition().x + (cloud2Speed * dt.asSeconds()),spriteCloud2.getPosition().y);
+                // Has the cloud reached the right hand edge of the screen?
+                if (spriteCloud2.getPosition().x > 1920)
+                {
+                // Set it up ready to be a whole new cloud next frame
+                cloud2Active = false;
+                }
+            }
+
+            // Cloud 3
+            if (!cloud3Active)
+            {
+                //How fast is the cloud
+                srand((int)time(0) * 30);
+                cloud3Speed = (rand() % 200);
+
+                // How high is the cloud
+                srand((int)time(0) * 30);
+                float height = (rand() % -450) - 150;
+                spriteCloud3.setPosition(-200, height);
+                cloud3Active = true;
+            }
+            else
+            {
+                spriteCloud3.setPosition(spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()),spriteCloud3.getPosition().y);
+                // Has the cloud reached the right hand edge of the screen?
+                if (spriteCloud3.getPosition().x > 1920)
+                {
+                // Set it up ready to be a whole new cloud next frame
+                cloud2Active = false;
+                }
+            }
             // draw scene
             window.draw(spriteBackground);
             window.draw(spriteCloud1);
